d2d59a0f6d928f2c230b116880883e9cef2e8445
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 17:42:20 -0400

    Remove StatusBar component import and instance

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -9,7 +8,6 @@ export default function App() {
   return (
     <View style={styles.screen} >
       <Header title="Guess a Number"/>
-      <StatusBar style="auto" />
     </View>
   );
 }
