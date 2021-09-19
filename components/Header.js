3f3b17a1d858faa839575f8c1edc8e4820062041
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 17:30:36 -0400

    Add boilerplate code for a Header component

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header;
