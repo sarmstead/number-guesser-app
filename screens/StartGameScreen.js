b4dd58d5d5e84640a327e8892d1679d345d105f3
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 17:45:18 -0400

    Add boilerplate code to StartGameScreen component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const StartGameScreen = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default StartGameScreen;
