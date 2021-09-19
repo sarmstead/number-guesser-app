6a5415a22ed7c9a34cc3acf0e51869cc70f1d037
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 17:50:10 -0400

    Return View and nested Text components. Add startGameScreen styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const StartGameScreen = props => {
-
+    return (
+       <View style={styles.startGameScreen}>
+           <Text>The Game Screen</Text>
+       </View> 
+    );
 }
 
 const styles = StyleSheet.create({
-
+    startGameScreen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    }
 })
 
 export default StartGameScreen;
