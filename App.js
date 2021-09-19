716b4166c26a944b8b8b7556c5f57c1fbe29cee3
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 17:39:24 -0400

    Import Header component

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+// Import components
+import Header from './components/Header';
+
 export default function App() {
   return (
     <View>
-      
+      <Header title="Guess a Number"/>
       <StatusBar style="auto" />
     </View>
   );
