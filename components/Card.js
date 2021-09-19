e6b5436af8bb72b383874a8d95f4644cbe7a38aa
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-09-19 18:32:56 -0400

    Paste StartGameScreen styles into Card component styles

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const Card = props => {
-
+    
 }
 
-styles=StyleSheet.create({
-    
+const styles=StyleSheet.create({
+    card: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.26,
+        shadowRadius: 6,
+        backgroundColor: '#ffffff',
+        elevation: 5,
+        padding: '7%',
+        borderRadius: 20
+    }
 })
 
 export default Card;
