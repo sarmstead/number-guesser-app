e7c2120be7c495363a613c0102ec043da0f98a21
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-10-24 19:23:42 -0400

    Configure TextInput to accept hard coded styles and styles from props.

--- components/Input.js
+++ components/Input.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
-const Input = props => {};
+const Input = props => {
+    return <TextInput style={{ ...styles.input, ...props.style }} />;
+};
 
+const styles = StyleSheet.create({
+
+})
 export default Input;
